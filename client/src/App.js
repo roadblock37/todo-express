2799c319c8128f5eae1cf1098b29e81ebd04ab4e
roadblock37 <Roadblock37@gmail.com>
2022-12-21 16:16:34 -0500

    file cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Todo from "./components/todo";
 import Header from "./components/header";
+import TodoContainer from "./components/todoContainer";
 
 function App() {
-  return <Header />;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
