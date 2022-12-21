fb78d1536eafd10615a57db730c4b9b6fd613c85
roadblock37 <Roadblock37@gmail.com>
2022-12-21 12:52:42 -0500

    created todo schema

---
+++ Models/todoModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxlength: [20, "name cannot be more than  20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
